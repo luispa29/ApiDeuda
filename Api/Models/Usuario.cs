e893a03358517be4d27eda146fd2caba7ddd58af
Luis Cevalllos <luispaulcevallos@gmail.com>
2024-02-10 19:34:07 -0500

    Add Tabla Deudores

---
+++ Api/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Correo { get; set; } = null!;
+
+    public bool Admin { get; set; }
+
+    public virtual ICollection<Deudore> Deudores { get; set; } = new List<Deudore>();
+}
