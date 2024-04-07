2439dafed18596283d9378232f377066a901dd23
Luis Cevalllos <luispaulcevallos@gmail.com>
2024-04-07 11:36:46 -0500

    add function codigoCompartido

--- Utilidades/Formatos.cs
+++ Utilidades/Formatos.cs
@@ -25,5 +25,21 @@ namespace Utilidades
             texto = texto.Replace(remplazar, textoReemplazo);
             return texto;
         }
+
+        public static string CodigoCompartido()
+        {
+            var characters = "0123456789";
+            var Charsarr = new char[4];
+            var random = new Random();
+
+            for (int i = 0; i < Charsarr.Length; i++)
+            {
+                Charsarr[i] = characters[random.Next(characters.Length)];
+            }
+
+            var resultString = new String(Charsarr);
+            return resultString;
+
+        }
     }
 }
