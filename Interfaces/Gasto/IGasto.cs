9f864c4f2a71ecf828236cd86a814b9d1b9357eb
Luis Cevalllos <luispaulcevallos@gmail.com>
2024-08-03 13:39:36 -0500

    Add funcion interfaces gasto

--- Interfaces/Gasto/IGasto.cs
+++ Interfaces/Gasto/IGasto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Modelos.Query.Prestamo;
+using Modelos.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace Interfaces.Gasto
 {
     public interface IGasto
     {
+        Task<GeneralResponse> Registrar(PrestamoQuery gasto, int idUsuario);
+
+        Task<GeneralResponse> Editar(PrestamoQuery gasto, int idUsuario);
+
+        Task<GeneralResponse> Eliminar(int idGasto, int idUsuario);
+
+        Task<GeneralResponse> ConsultarGastos(int pagina, int registros, int idUsuario, DateOnly? fechaDesde, DateOnly? fechaHasta);
     }
 }
--- Interfaces/Gasto/IGastoLogica.cs
+++ Interfaces/Gasto/IGastoLogica.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Modelos.Query.Prestamo;
+using Modelos.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Interfaces.Gasto
 {
-    public class IGastoLogica
+    public interface IGastoLogica
     {
+        Task<GeneralResponse> Registrar(PrestamoQuery gasto, string token);
+       
+        Task<GeneralResponse> Editar(PrestamoQuery gasto, string token);
+
+        Task<GeneralResponse> Consultar(int pagina, int registros, string token, DateTime? fechaDesde, DateTime? fechaHasta);
+
+        Task<GeneralResponse> Eliminar(int idPrestamo, string token);
     }
 }
