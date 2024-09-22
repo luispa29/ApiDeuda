8f355de2ac7d48dd8a11f21a21cc3ab5d73a7f76
Luis Cevalllos <luispaulcevallos@gmail.com>
2024-09-21 20:51:43 -0500

    Add function ExistePresupuesto

--- Interfaces/Presupuesto/IPresupuesto.cs
+++ Interfaces/Presupuesto/IPresupuesto.cs
@@ -8,5 +8,6 @@ namespace Interfaces.Presupuesto
 {
     public interface IPresupuesto
     {
+        Task<bool> ExistePresupuesto(int usuarioId, int mes, int anio);
     }
 }
--- Servicios/Presupuesto/PresupuestoService.cs
+++ Servicios/Presupuesto/PresupuestoService.cs
@@ -1,5 +1,6 @@
 ﻿using DBEF.Models;
 using Interfaces.Presupuesto;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,11 @@ namespace Servicios.Presupuesto
     public class PresupuestoService (AppDeudaContext bd) : IPresupuesto
     {
         private readonly AppDeudaContext _bd = bd;
+
+        public async Task<bool> ExistePresupuesto(int usuarioId, int mes, int anio)
+        {
+            try { return await _bd.Presupuestos.Where(p => p.UsuarioId == usuarioId && p.Mes == mes && p.Anio == anio).AnyAsync(); }
+            catch (Exception) { return false; }
+        }
     }
 }
